ff711f6960b2935c762503d42dabe1f6c648275e
jjmich <kubalakloss@gmail.com>
2020-06-16 00:35:22 +0200

    First commit

---
+++ api/controllers/ArticlesController.js
@@ -0,0 +1,70 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+
+
+module.exports = {
+    list: function(req, res){
+        Articles.find({}).exec(function(err, articles){
+            if(err){
+                res.status(500).send({error: 'Database Error'});
+            }
+            res.view('pages/list', {articles:articles})
+        })
+    },
+    add: function(req, res){
+        res.view('pages/add');
+    },
+
+    create:function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.create({title:title, body:body}).exec(function(err){
+            if(err){
+                res.status(500).send({error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+    },
+    destroy: function(req, res){
+        Articles.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.status(500).send({error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Articles.findOne({id:req.params.id}).exec(function(err, article){
+            if(err){
+                res.status(500).send({error: 'Database Error'});
+            }
+
+            res.view('pages/edit', {article:article});
+        });
+    },
+    update: function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.update({id:req.params.id},{title:title, body:body}).exec(function(err){
+            if(err){
+                res.status(500).send({error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+        
+        return false;
+    }
+};
+
